252eae07f17fda5a01add6b9e3b9339f1c6d7004
Ahmed Lajmi <ahmed.lajmi@ensi-uma.tn>
2019-01-11 22:07:23 +0100

    initial commit

---
+++ principalApp/urls.py
@@ -0,0 +1,17 @@
+"""crepes_bretonnes URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/dev/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+[...]
+"""
+from django.contrib import admin
+from django.urls import path
+from principalApp import views
+
+urlpatterns = [
+    path('', views.index),
+]
